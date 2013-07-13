4f868e54fe1f1c54397ed67984ef0eaf25de0185
blibotics <blibotics@gmail.com>
2013-07-13 19:53:06 -0400

    Creating loop in visitor_spec for FactoryGirl

--- spec/features/visitor_spec.rb
+++ spec/features/visitor_spec.rb
@@ -4,6 +4,9 @@ describe 'Visitor' do  #describes the 'who'
   context 'visits the front page' do #context does the 'what' the 'who' is doing
     it {should visit root_path} #it, is the test
     it 'displays content on page' do
+      (1..3).each do |x|
+        FactoryGirl.create(:product)
+      end
       visit root_path
       page.should have_selector('table')
       page.all('tr').should have(3).items
