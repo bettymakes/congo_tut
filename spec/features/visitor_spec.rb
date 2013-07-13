3f5ac234c62e380f6912ee1cdeff976581156486
blibotics <blibotics@gmail.com>
2013-07-13 17:08:35 -0400

    Writing test to test that there is content on the page

--- spec/features/visitor_spec.rb
+++ spec/features/visitor_spec.rb
@@ -3,5 +3,9 @@ require 'spec_helper'
 describe 'Visitor' do  #describes the 'who'
   context 'visits the front page' do #context does the 'what' the 'who' is doing
     it {should visit root_path} #it, is the test
+    it 'displays content on page' do
+      visit root_path
+      page.should have_selector('table')
+    end
   end
 end
