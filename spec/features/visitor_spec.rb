a8e720c773e7b21cb165544ac048accf8ec1e923
blibotics <blibotics@gmail.com>
2013-07-13 16:37:30 -0400

    Writing first test

--- spec/features/visitor_spec.rb
+++ spec/features/visitor_spec.rb
@@ -1,1 +1,7 @@
-visitor_spec.rb
+require 'spec_helper'
+
+describe 'Visitor' do  #describes the 'who'
+  context 'visits the front page' do #context does the 'what' the 'who' is doing
+    it {should visit root_path} #it, is the test
+  end
+end
