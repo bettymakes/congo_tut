7304f0eb3484cce115a217b5c7839e1651798bf5
blibotics <blibotics@gmail.com>
2013-07-13 20:25:37 -0400

    Adding td
    end

--- spec/features/visitor_spec.rb
+++ spec/features/visitor_spec.rb
@@ -4,9 +4,10 @@ describe 'Visitor' do  #describes the 'who'
   context 'visits the front page' do #context does the 'what' the 'who' is doing
     it {should visit root_path} #it, is the test
     it 'displays content on page' do
-      (1..3).each do |x|
+      (1..2).each do |x|
         product = FactoryGirl.create(:product)
       end
+      product = FactoryGirl.create(:product)
       visit root_path
       page.should have_selector('table')
       page.all('tr').should have(3).items
